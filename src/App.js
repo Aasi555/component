ccc6b680258082d580cbe944001753d182272e92
Aasif Dange <97660014+Aasi555@users.noreply.github.com>
2022-02-06 12:24:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div id="root" className="App">
+      <p>
+        <h1>I am learning React. My life is getting better. </h1>
+      </p>
+    </div>
+  );
+}
