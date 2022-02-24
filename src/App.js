eb26f52dbea8f64a7d6aab43c77279932787557b
Aasi555 <dangeaasif30@gmail.com>
2022-02-24 12:21:37 +0530

    1

--- src/App.js
+++ src/App.js
@@ -14,10 +14,22 @@ export default App;*/
 require("./styles.css");
 
 function App() {
-  return /*#__PURE__*/React.createElement("div", {
-    id: "root",
-    className: "App"
-  }, /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("h1", null, "I am learning React. My life is getting better. ")));
+  return /*#__PURE__*/ React.createElement(
+    "div",
+    {
+      id: "root",
+      className: "App",
+    },
+    /*#__PURE__*/ React.createElement(
+      "p",
+      null,
+      /*#__PURE__*/ React.createElement(
+        "h1",
+        null,
+        "I am learning React. My life is getting better. "
+      )
+    )
+  );
 }
 
 var _default = App;
